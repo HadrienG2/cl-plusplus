2ba5e640edcc2af323c2dbd5a957114e85d272a8
Hadrien G <knights_of_ni@gmx.com>
2015-11-15 17:46:44 +0100

    Added a first base class draft for memory objects

---
+++ CLplusplus/memory_object.cpp
@@ -0,0 +1,88 @@
+#include "common.hpp"
+#include "memory_object.hpp"
+
+namespace CLplusplus {
+
+   MemoryObject::MemoryObject(const MemoryObject & source) {
+      // Whenever a copy of a reference-counted memory object is made, its reference count should be incremented
+      copy_internal_data(source);
+      retain();
+   }
+
+   MemoryObject & MemoryObject::operator=(const MemoryObject & source) {
+      // Reference count considerations also apply to copy assignment operator
+      copy_internal_data(source);
+      retain();
+      return *this;
+   }
+
+   void MemoryObject::set_destructor_callback(const DestructorCallback & callback) {
+      add_destructor_callback(callback);
+   }
+
+   void MemoryObject::set_destructor_callback(const DestructorCallbackWithUserData & callback, void * const user_data) {
+      add_destructor_callback(make_destructor_callback(callback, user_data));
+   }
+
+   size_t MemoryObject::raw_query_output_size(const cl_mem_info parameter_name) const {
+      size_t result;
+      raw_query(parameter_name, 0, nullptr, &result);
+      return result;
+   }
+
+   void MemoryObject::raw_query(const cl_mem_info parameter_name, const size_t output_storage_size, void * output_storage, size_t * actual_output_size) const {
+      throw_if_failed(clGetMemObjectInfo(internal_id, parameter_name, output_storage_size, output_storage, actual_output_size));
+   }
+
+   MemoryObject::MemoryObject(const cl_mem identifier, const bool increment_reference_count) :
+      internal_id{identifier},
+      internal_callbacks_ptr{new std::vector<DestructorCallback>}
+   {
+      // Handle invalid memory object IDs
+      if(internal_id == NULL) throw InvalidArgument();
+      
+      // Unless asked not to do so, increment the memory object's reference count
+      if(increment_reference_count) retain();
+   }
+
+   MemoryObject::DestructorCallback MemoryObject::make_destructor_callback(const DestructorCallbackWithUserData & callback, void * const user_data) {
+      using namespace std::placeholders;
+      return DestructorCallback{std::bind(callback, _1, user_data)};
+   }
+
+   void MemoryObject::add_destructor_callback(const DestructorCallback & callback) {
+      // First, we must add the destructor callback to our shared callback database
+      if(callback) internal_callbacks_ptr->emplace_back(callback);
+
+      // If we just added our first destructor callback, we must also register our catch-all static callback to OpenCL
+      if(internal_callbacks_ptr->size() == 1) {
+         throw_if_failed(clSetMemObjectDestructorCallback(internal_id, raw_callback, static_cast<void *>(internal_callbacks_ptr)));
+      }
+   }
+
+   void CL_CALLBACK MemoryObject::raw_callback(cl_mem memobj, void * actual_callbacks_ptr) {
+      // Extract the destructor callback vector from the raw void * pointer we received
+      const auto actual_callbacks = *(static_cast<const std::vector<DestructorCallback> *>(actual_callbacks_ptr));
+
+      // Call the destructor callbacks in reverse order (per OpenCL specification)
+      for(int i = actual_callbacks.size() - 1; i >= 0; --i) {
+         actual_callbacks[i](memobj);
+      }
+   }
+
+   void MemoryObject::copy_internal_data(const MemoryObject & source) {
+      internal_id = source.internal_id;
+      internal_callbacks_ptr = source.internal_callbacks_ptr;
+   }
+
+   void MemoryObject::retain() const {
+      throw_if_failed(clRetainMemObject(internal_id));
+   }
+
+   void MemoryObject::release() {
+      bool last_reference = (reference_count() == 1);
+      throw_if_failed(clReleaseMemObject(internal_id));
+      if(last_reference && internal_callbacks_ptr) delete internal_callbacks_ptr;
+   }
+
+}
---
+++ CLplusplus/memory_object.hpp
@@ -0,0 +1,107 @@
+// Copyright 2015 Hadrien Grasland
+//
+// This file is part of CLplusplus.
+//
+// CLplusplus is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLplusplus is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CLplusplus.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef INCLUDE_CL_PLUSPLUS_MEMORY_OBJECT
+#define INCLUDE_CL_PLUSPLUS_MEMORY_OBJECT
+
+#include <functional>
+#include <vector>
+
+#include <CL/cl.h>
+
+// This code unit defines a base class for all OpenCL memory objects : buffers, images...
+namespace CLplusplus {
+
+   class MemoryObject {
+      public:
+         // Memory objects are reference counted using the following functions
+         MemoryObject(const MemoryObject & source);
+         virtual ~MemoryObject() { release(); }
+         MemoryObject & operator=(const MemoryObject & source);
+
+         // It is possible to register a stack of callbacks to be called when memory objects will be destroyed. This is typically useful for shared host buffer liberation.
+         // We accept native std::functions for this purpose, with and without user-defined data blocks.
+         // We discourage the use of such data blocks in C++11 as lambdas and std::bind() usually provide a safer alternative, but they are needed for legacy C code compatibility.
+         using DestructorCallback = std::function<void(cl_mem)>;
+         using DestructorCallbackWithUserData = std::function<void(cl_mem, void *)>;
+
+         // Here are the functions used to set such callbacks
+         void set_destructor_callback(const DestructorCallback & callback);
+         void set_destructor_callback(const DestructorCallbackWithUserData & callback, void * const user_data);
+
+         // Memory object properties which are supported by the wrapper are directly accessible in a convenient, high-level fashion
+         cl_mem_object_type type() const { return raw_value_query<cl_mem_object_type>(CL_MEM_TYPE); }
+         cl_mem_flags flags() const { return raw_value_query<cl_mem_flags>(CL_MEM_FLAGS); }
+         size_t size() const { return raw_size_query(CL_MEM_SIZE); }
+         void * host_ptr() const { return raw_value_query<void*>(CL_MEM_HOST_PTR); }
+         cl_uint map_count() const { return raw_uint_query(CL_MEM_MAP_COUNT); }
+
+         bool has_associated_memobject() const { return (raw_associated_memobject() != NULL); }
+         CLplusplus::MemoryObject associated_memobject() const { return MemoryObject{raw_associated_memobject(), true}; }
+         size_t offset() const { return raw_size_query(CL_MEM_OFFSET); }
+
+         // Unsupported property values may be queries in a lower-level way
+         cl_context raw_context_id() const { return raw_value_query<cl_context>(CL_QUEUE_CONTEXT); } // NOTE : Returning a Context here would lead to a circular dependency.
+                                                                                                     // WARNING : Beware that trying to use this identifier in a Context can lead to callback memory leaks.
+         cl_mem raw_associated_memobject() const { return raw_value_query<cl_mem>(CL_MEM_ASSOCIATED_MEMOBJECT); }
+
+         // And unsupported memory object properties can be queried in a nearly pure OpenCL way, with some common-case usability optimizations
+         size_t raw_size_query(const cl_mem_info parameter_name) const { return raw_value_query<size_t>(parameter_name); }
+         cl_uint raw_uint_query(const cl_mem_info parameter_name) const { return raw_value_query<cl_uint>(parameter_name); }
+
+         template<typename ValueType> ValueType raw_value_query(const cl_mem_info parameter_name) const {
+            ValueType result;
+            raw_query(parameter_name, sizeof(ValueType), &result);
+            return result;
+         }
+
+         size_t raw_query_output_size(const cl_mem_info parameter_name) const;
+         void raw_query(const cl_mem_info parameter_name, const size_t output_storage_size, void * output_storage, size_t * actual_output_size = nullptr) const;
+
+         // TODO : Provide CommandQueue with a way to unmap memory objects
+         // TODO : Provide CommandQueue with a way to migrate memory objects
+
+         // Finally, if the need arises, one can directly access the memory object identifier in order to perform raw OpenCL operations.
+         // WARNING : Be very careful when you do this, as such raw identifiers will NOT be taken into account during reference counting !
+         cl_mem raw_identifier() const { return internal_id; }
+
+      protected:
+         // This is the internal identifier that represents our memory object
+         cl_mem internal_id;
+
+         // Memory objects can be created from a valid OpenCL identifier, but we should let child classes take care of that.
+         MemoryObject(const cl_mem identifier, const bool increment_reference_count);
+
+      private:
+         // In general, we DO NOT want to deal with multiple kinds of callbacks, so the user data version is converted to a regular callback as soon as possible
+         static DestructorCallback make_destructor_callback(const DestructorCallbackWithUserData & callback, void * const user_data);
+
+         // High-level destructor callbacks are stored here. They will be called by a lower-level static function which follows OpenCL's linkage conventions.
+         std::vector<DestructorCallback> * internal_callbacks_ptr;
+         void add_destructor_callback(const DestructorCallback & callback);
+         static void CL_CALLBACK raw_callback(cl_mem memobj, void * actual_callbacks_ptr);
+
+         // These functions manage the life cycle of reference-counted memory objects
+         void copy_internal_data(const MemoryObject & source);
+         cl_uint reference_count() const { return raw_uint_query(CL_MEM_REFERENCE_COUNT); }
+         void retain() const;
+         void release();
+   };
+
+}
+
+#endif
