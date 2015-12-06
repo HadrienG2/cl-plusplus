daf8cb2057b5d5484f3560f1cf66d985d8e2c55a
Hadrien G <knights_of_ni@gmx.com>
2015-12-06 19:22:13 +0100

    Split the buffer object example into two smaller ones

--- clpp_buffer_readwrite.cpp
+++ clpp_buffer_readwrite.cpp
@@ -43,7 +43,9 @@ int main() {
       },
       [&](const CLplusplus::Device & device) -> bool {
          if(device.version() < target_version) return false;                  // OpenCL platforms may support older-generation devices, which we need to eliminate
+         const bool device_supports_ooe_execution = device.queue_properties() & CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE;
          return device.available() &&                                         // Device is available for compute purposes
+                device_supports_ooe_execution &&                              // Device can execute OpenCL commands out of order
                 (device.max_mem_alloc_size() >= min_mem_alloc_size) &&        // Device accepts large enough global memory allocations
                 (device.global_mem_size() >= min_global_mem_size);            // Device has enough global memory
       }
@@ -53,68 +55,44 @@ int main() {
    const auto context = Shared::build_default_context(selected_platform_and_device);
 
    // Create a small OpenCL buffer
-   auto mutable_buffer = context.create_buffer(CL_MEM_READ_WRITE, buffer_size);
+   const auto buffer = context.create_buffer(CL_MEM_READ_WRITE, buffer_size);
 
-   // Set a callback to be called on buffer destruction, then switch to a safer immutable buffer object view
-   mutable_buffer.set_destructor_callback(
-      [](cl_mem memory_object) {
-         std::cout << std::endl << "The buffer will now be destroyed" << std::endl;
+   // Create an out-of-order command queue for the device
+   const auto command_queue = context.create_command_queue(CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE);
+
+   // Prepare some input data to be sent to the buffer
+   cl_uchar input[buffer_size];
+   for(size_t i = 0; i < buffer_size; ++i) input[i] = (255 - i) % 256;
+
+   // Schedule to send the input data to the buffer. Here, we use the "enqueued" syntax, which gives us access to the associated OpenCL event.
+   auto send_event = command_queue.enqueued_write_buffer(static_cast<const void *>(input), false, buffer, 0, buffer_size, {});
+   send_event.set_callback(CL_COMPLETE,
+      [](cl_event unused, cl_int command_status) {
+         if(command_status == CL_COMPLETE) {
+            std::cout << "Input data has been successfully written to the buffer" << std::endl;
+         } else {
+            std::cout << "An error occurred while sending input data" << std::endl;
+         }
       }
    );
-   const auto & buffer = mutable_buffer;
 
-   // Display the memory object properties of our buffer
-   std::cout << "Our newly created buffer is ";
-   if(buffer.type() == CL_MEM_OBJECT_BUFFER) {
-      std::cout << "a perfectly normal OpenCL buffer";
-   } else {
-      std::cout << "something strange and unforeseen";
+   // Schedule to get the data back. Note that here, we use a different syntax ("enqueue") which does not give us access to the read event.
+   // This may be handy for quick coding, but the lack of error handling makes use of this variant somewhat unadvisable in production code.
+   cl_uchar output[buffer_size];
+   command_queue.enqueue_read_buffer(buffer, 0, static_cast<void *>(output), buffer_size, {send_event});
+
+   // Wait for all pending commands to finish
+   command_queue.finish();
+   std::cout << "Output data should now be fetched back from the buffer" << std::endl;
+
+   // Compare the input and output buffers' contents
+   for(size_t i = 0; i < buffer_size; ++i) {
+      if(input[i] != output[i]) {
+         std::cout << "Data transmission failed !" << std::endl;
+         std::abort();
+      }
    }
-   std::cout << std::endl;
-
-   const auto flags = buffer.flags();
-   std::cout << "Its flags are ";
-   if(flags & CL_MEM_WRITE_ONLY) std::cout << "WRITE_ONLY ";
-   else if(flags & CL_MEM_READ_ONLY) std::cout << "READ_ONLY ";
-   else std::cout << "READ_WRITE ";
-   if(flags & CL_MEM_USE_HOST_PTR) std::cout << "USE_HOST_PTR ";
-   if(flags & CL_MEM_ALLOC_HOST_PTR) std::cout << "ALLOC_HOST_PTR ";
-   if(flags & CL_MEM_COPY_HOST_PTR) std::cout << "COPY_HOST_PTR ";
-   if(flags & CL_MEM_HOST_WRITE_ONLY) std::cout << "HOST_WRITE_ONLY";
-   if(flags & CL_MEM_HOST_READ_ONLY) std::cout << "HOST_READ_ONLY";
-   if(flags & CL_MEM_HOST_NO_ACCESS) std::cout << "HOST_NO_ACCESS";
-   std::cout << std::endl;
-
-   std::cout << "It is " << buffer.size() << " bytes long" << std::endl;
-
-   const auto host_ptr = buffer.host_ptr();
-   std::cout << "Its host pointer points to address ";
-   if(host_ptr == nullptr) {
-      std::cout << "NULL";
-   } else {
-      std::cout << (size_t)(void *)host_ptr;
-   }
-   std::cout << std::endl;
-
-   std::cout << "Our buffer is currently being mapped " << buffer.map_count() << " times" << std::endl;
-
-   std::cout << "Our buffer ";
-   if(buffer.raw_context_id() == context.raw_identifier()) {
-      std::cout << "identifies with the right context";
-   } else {
-      std::cout << "seems to deny its context of origin, which is problematic";
-   }
-   std::cout << std::endl;
-
-   std::cout << "This is ";
-   if(buffer.has_associated_memobject() == false) {
-      std::cout << "a top-level buffer";
-   } else {
-      std::cout << "a sub-buffer";
-   }
-   std::cout << std::endl;
-
-   std::cout << "Our buffer's internal offset is " << buffer.offset() << std::endl;
+   std::cout << "Data was transmitted successfully" << std::endl;
 
    return 0;
 }
